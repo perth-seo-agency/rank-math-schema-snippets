7d834cb75498fd1cfbfbe65fdace4e7823997c6f
Daniel Emery <86828899+perth-seo-agency@users.noreply.github.com>
2024-09-16 22:14:27 +1000

    Update change-google-maps-url.php

--- change-google-maps-url.php
+++ change-google-maps-url.php
@@ -7,18 +7,18 @@
  * However, it should use the CID URL vs just using the coordinates.
  */
 
-// Add custom hasMap property using the Google Maps Profile's CID URL
-add_filter( 'rank_math/json_ld', function( $data, $jsonld ) {
-	if ( isset($data['publisher']) ) {
-		$data['publisher']['hasMap'] = 'https://www.google.com/maps?cid=123456789';
-	}
-	return $data;
-}, 99, 2);
-
 // Remove the defaul Rank Math "coordinates"
 add_filter( 'rank_math/json_ld', function( $data, $jsonld ) {
     if ( isset( $data['place'] ) && isset( $data['place']['hasMap'] ) ) {
         unset( $data['place']['hasMap'] );
     }
     return $data;
+}, 99, 2);
+
+// Add custom hasMap property using the Google Maps Profile's CID URL
+add_filter( 'rank_math/json_ld', function( $data, $jsonld ) {
+	if ( isset($data['place']) ) {
+		$data['place']['hasMap'] = 'https://www.google.com/maps?cid=17018878135737943086';
+	}
+	return $data;
 }, 99, 2);
